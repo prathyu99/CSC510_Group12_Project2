015e0c19222b7e3096c3acf0dddfc098a08e0a44
Aravinda Raman J <aravindaraman04@gmail.com>
2023-10-19 15:29:28 -0400

    added delete menu functionality

--- src/components/Job1.js
+++ src/components/Job1.js
@@ -1,16 +1,17 @@
-
-
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { deleteMenu, fetchMenus } from '../actions/job';
 
 
 class Job1 extends Component {
-    
-    
+    deleteItem = (menuname)=>{
+    this.props.dispatch(deleteMenu(menuname));
+    alert("Are sure, you want to delete "+ menuname +" from your menu?");
+    this.props.dispatch(fetchMenus());
+  }
 
-      
-    
     render() {
         const { job } = this.props;
         const {menu } = this.props;
@@ -26,6 +27,8 @@ class Job1 extends Component {
               <div >
                 <h4 style={{display:'inline-block'}}>Restaurant Name : </h4> 
                 <span style={{marginLeft:'10px'}}>{menu.restname}</span>
+                <button style={{marginLeft:'300px'}} className="button delete-btn" onClick={()=>this.deleteItem(menu.menuname)}>Delete &nbsp; 
+                <FontAwesomeIcon icon={faTrash} style={{color: "#f9fafa",}}/></button>
                   </div>
 
 
@@ -66,5 +69,3 @@ function mapStateToProps({ auth, job,application }) {
   }
   
   export default connect(mapStateToProps)(Job1);
-
-
